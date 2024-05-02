4b1e4ec7668bc116cd8c25837fc3d5457bc4b460
gabsnim <70450036+gabsnim@users.noreply.github.com>
2024-05-02 19:17:05 -0300

    ed08

---
+++ 08/ed08.c
@@ -0,0 +1,844 @@
+/*
+Aluno: Gabriel Xavier Borges
+Matricula: 805347
+*/
+
+#include "gxb.h"
+#include "io.h"
+
+// void exercicio01 (int n, int array [])
+// {
+    
+// }
+void method_01(void) 
+{
+    int n = 0;
+    int *array = NULL;
+    int x = 0;
+
+    do
+    {
+        n = G_readint("Digite um valor para o tamanho do arranjo: ");
+    } while (n < 1);
+
+    // array = (int *)malloc(n * sizeof(int));
+    array = malloc(n * sizeof(int));
+
+    if(array == NULL)
+    {
+        printf("ERRO ao alocar memoria para o array\n");
+    }
+    else
+    {   
+        for(int i = 0; i < n; i++)
+        {
+            do
+            {
+                printf("Elemento %d = ", i);
+                x = G_readint("");
+            } while (x % 2 != 0 && x > 0);
+            array[i] = x;
+        }
+        G_printarray(n, array);
+
+        free(array);
+    }
+}
+
+// Exercicio 02
+
+void exercicio02 (chars Filename, int *array)
+{
+    // FILE* arquivo = fopen (Filename, "rt");  //arquivo para leitura 
+    // FILE* arquivo1 = fopen ("DADOS1.TXT", "wt");  //arquivo para escrita
+    // int i = 0;
+    // int y = 0;
+    // int x = 0;
+
+
+    // if(arquivo1 == NULL || arquivo == NULL)
+    // {
+    //     printf("ERRO ao abrir arquivo(s)\n");
+    // }
+    // else
+    // {
+    //     fscanf(arquivo, "%d", &n);
+
+    //     if(n > 1)
+    //     {
+            
+    //     }
+    //     array = malloc(n * sizeof(int));
+
+    //     if(array == NULL)
+    //     {
+    //         printf("ERRO ao alocar memoria para o arranjo\n");
+    //     }
+
+    //     while (! feof(arquivo) && i < n)
+    //     {
+    //         fscanf(arquivo, "%d", &y);
+    //         array[i] = y;               // armazenar
+    //         i++;
+    //     }
+    //     // G_printarray(n, array);
+
+    //     for(int j = 0; j < n; j++)
+    //     {
+    //         if(array[j] % 2 != 0 && array[j] > 0)
+    //         {
+    //             x = array[j];
+    //             fprintf(arquivo1, "%d\n", x);
+    //         }
+    //     }
+    // }
+    // fclose(arquivo1);
+    // fclose(arquivo);
+}
+void method_02(void)
+{
+    // int *array = NULL;
+    // int n = 0;
+
+    // do
+    // {
+    //     G_readint("Digite um valor para o tamanho do array: \n");
+    // } while (1 < n);
+    
+    // array = malloc(n * sizeof(int));
+
+    // if(array == NULL)
+    // {
+    //     printf("ERRO ao alocar memoria para o arranjo\n");
+    // }
+    // else
+    // {
+    //     exercicio02("LEITURA1.TXT", n, array);
+    // }
+    // exercicio02("LEITURA1.TXT", array);
+    
+}
+
+void exercicio03 (int a, int b, int n)
+{
+  FILE* arquivo = fopen("DADOS.TXT", "wt");
+  int *array = NULL;
+  int i = 0;
+  int x = 0;
+
+  array = malloc(n * sizeof(int));
+
+  if(array == NULL)
+  {
+    printf("ERRO ao alocar memoria para arranjo\n");
+  }
+  else
+  {
+    while(i < n)
+    {
+      x = rand() % 100;
+      if(x >= a && x <= b)
+      {
+        array[i] = x;
+        i++;
+      }
+    }
+  }
+  // G_printarray(n, array);
+  if(arquivo == NULL)
+  {
+    G_printERROFILE();
+  }
+  else
+  {
+    fprintf(arquivo, "%d\n", n);
+    for(int j = 0; j < n; j++)
+    {
+      fprintf(arquivo, "%d\n", array[j]);
+    }
+    printf("Dados gravados com sucesso em DADOS.TXT\n");
+  }
+  fclose(arquivo);
+}
+void method_03(void)
+{
+  int a = G_readint("Digite o inferior do intervalo: ");
+  int b = G_readint("Digite o superior do intervalo: ");
+  int n = G_readint("Digite o valor de numeros a serem gerados: ");
+  exercicio03(a, b, n);
+}
+
+int *exercicio04 (chars Filename, int *ref)  // ler os dados do arquivo, passar como referencia o tamanho do arranjo e retornar o arranjo
+{
+  FILE* arquivo = fopen(Filename, "rt");
+  int *array = NULL;
+  int n = 0;
+  int x = 0;
+  int y = 0;
+
+  if(arquivo == NULL)
+  {
+    G_printERROFILE();
+  }
+  else
+  {
+    fscanf(arquivo, "%d", &n);
+    *ref = n;
+    array = malloc(n * sizeof(int));
+    if(array == NULL)
+    {
+      printf("ERRO ao alocar memoria para arranjo.\n");
+    }
+    else
+    {
+      while(!feof (arquivo) && x < n)
+      {
+        fscanf(arquivo, "%d", &y);
+        array[x] = y;
+        x++;
+      }
+    }
+  }
+  fclose(arquivo);
+  // G_printarray(n, array);
+  return ( array );
+}
+int exercicio04_1(int *array, int n)  // retornar o menor valor impar do arranjo
+{
+  int x = 0;
+  int y = 0;
+  y = array[0];
+  for(int i = 0; i < n; i++)
+  {
+    if(array[i] % 2 != 0)
+    {
+      x = array[i];
+      if(y > x)
+      {
+        y = x;
+      }
+    }
+  }
+  return y;
+}
+void method_04(void)
+{
+  int n = 0;
+  int *array = exercicio04("DADOS.TXT", &n);
+  int resposta = exercicio04_1(array, n);
+  printf("menor valor impar do arranjo = %d\n", resposta);
+}
+
+int *exercicio05 (chars Filename, int *ref) // ler os dados do arquivo, passar como referencia o tamanho do arranjo e retornar o arranjo
+{
+  FILE* arquivo = fopen (Filename, "rt");
+  int *array = NULL;
+  int n = 0;
+  int x = 0;
+  int y = 0;
+
+  if(arquivo == NULL)
+  {
+    G_printERROFILE();
+  }
+  else
+  {
+    fscanf(arquivo, "%d", &n);
+    *ref = n;
+    array = malloc(n * sizeof(int));
+    if(array == NULL)
+    {
+      printf("ERRO ao alocar memoria para arranjo.\n");
+    }
+    else
+    {
+      while(!feof(arquivo) && x < n)
+      {
+        fscanf(arquivo, "%d", &y);
+        array[x] = y;
+        x++;      
+      }
+    }
+  }
+  fclose(arquivo);
+  return (array);
+}
+int exercicio05_1 (int *array, int n) // retornar o maior valor impar do arranjo
+{
+  int y = 0;
+  int x = 0;
+
+  for(int i = 0; i < n; i++)
+  {
+    if(array[i] % 2 != 0)
+    {
+      x = array[i];
+      if(y < x)
+      {
+        y = x;
+      }  
+    }
+  }
+  return y;
+}
+void method_05(void)
+{
+  int n = 0;
+  int *array = exercicio05("DADOS.TXT", &n);
+  int resposta = exercicio05_1(array, n);
+  // G_printarray(n, array);
+  printf("maior valor impar do arranjo = %d\n", resposta);
+}
+
+
+int *exercicio06 (chars Filename, int *ref) // ler os dados do arquivo, passar o tamanho como referencia e retornar o arranjo
+{
+  FILE* arquivo = fopen (Filename, "rt");
+  int *array = NULL;
+  int n = 0;
+  int x = 0;
+  int y = 0;
+
+  if(arquivo == NULL)
+  {
+    G_printERROFILE();
+  }
+  else
+  {
+    fscanf(arquivo, "%d", &n);
+    *ref = n;
+    array = malloc(n * sizeof(int));
+    if(array == NULL)
+    {
+      printf("ERRO ao alocar memoria para arranjo.\n");
+    }
+    else
+    {
+      while(!feof(arquivo) && n > x)
+      {
+        fscanf(arquivo, "%d", &y);
+        array[x] = y;
+        x++;
+      }
+    }
+  }
+  // G_printarray(n, array);
+  fclose (arquivo);
+  return (array);
+}
+
+int exercicio06_1 (int *array, int n) // achar e retornar a media  
+{
+  int media = 0;
+
+  for(int i = 0; i < n; i++)
+  {
+    media += array[i];
+  }
+  media = media / n;
+
+  return (media);
+}
+
+void method_06(void) // gravar os valores em arquivos
+{
+  int n = 0;
+  int *array = exercicio06("DADOS.TXT", &n);
+  
+  int media = exercicio06_1(array, n);
+  int x = 0;
+  // G_printarray(n, array);
+
+  FILE* arquivo1 = fopen ("EXERCICIO06_1.TXT", "wt");
+  FILE* arquivo2 = fopen ("EXERCICIO06_2.TXT", "wt");
+
+  if(arquivo1 == NULL || arquivo2 == NULL)
+  {
+    printf("ERRO ao abrir arquivo(s).\n");
+  }
+  else
+  {
+    for(int i = 0; i < n; i++)
+    {
+      x = array[i];
+      if(array[i] <= media)
+      {
+        fprintf(arquivo1, "%d\n", x);
+      }
+      else
+      {
+        fprintf(arquivo2, "%d\n", x);
+      }
+    }
+  }
+  fclose(arquivo1);
+  fclose(arquivo2);
+  printf("Valores <= a media gravados em EXERCICIO06_1\n");
+  printf("Valores > a media gravados em EXERCICIO06_2\n");
+}
+
+bool exercicio07 (int *array, int n)
+{
+  int count = 0;
+  bool resposta = false;
+
+  for(int i = 1; i < n; i++)
+  {
+    if(array[i] > array[i - 1])
+    {
+      count++;
+    }
+  }
+  if(count == 0)
+  {
+    resposta = true;
+  }
+  return resposta;
+}
+
+void method_07(void)
+{
+  int *array = NULL;
+  int n = G_readint("Digite o tamanho do array: ");
+  array = malloc(n * sizeof(int));
+
+  G_readarray(n, array);
+
+  bool resposta = exercicio07(array, n);
+  // exercicio07(array, n);
+
+  if(resposta == true)
+  {
+    printf("O array esta em ordem decrescente. ");
+    G_printarray(n, array);
+  }
+  else
+  {
+    printf("O array nao esta em ordem decrescente. ");
+    G_printarray(n, array);
+  }
+}
+
+int *exercicio08 (chars Filename, int *ref) //ler dados do arquivo e guardar num arranjo
+{
+  FILE* arquivo = fopen (Filename, "rt");
+  int *array = NULL;
+  int n = 0;
+  int x = 0;
+  int y = 0;
+
+  if(arquivo == NULL)
+  {
+    G_printERROFILE();
+  }
+  else
+  {
+    fscanf(arquivo, "%d", &n);
+    *ref = n;
+    array = malloc(n * sizeof(int));
+
+    if(array == NULL)
+    {
+      printf("ERRO ao alocar memoria para arranjo. ");
+    }
+    else
+    {
+      while(!feof(arquivo) && x < n)
+      {
+        fscanf(arquivo, "%d", &y);
+        array[x] = y;
+        x++;
+      }
+    }
+  }
+  fclose(arquivo);
+  return (array);
+}
+
+bool exercicio08_1 (int *array, int n, int valor, int posicao)
+{
+  bool resposta = true;
+  int count = 0;
+  int i = posicao;
+
+  while(i < n)
+  {
+    if(array[i] == valor)
+    {
+      count++;
+    }
+    i++;
+  }
+
+  if(count == 0)
+  {
+    resposta = false;
+  }
+
+  return (resposta);
+}
+void method_08(void)
+{ 
+  int *array = NULL;
+  int n = 0;
+  array = exercicio08("DADOS.TXT", &n);
+
+  int valor = G_readint("Digite o valor a ser procurado no arranjo: ");
+  int posicao = G_readint("Digite a posicao para comecar a procura: ");
+
+  bool resposta = exercicio08_1(array, n, valor, posicao);
+
+  for(int i = 0; i < n; i++)
+  {
+    printf("posicao %d = %d\n",i, array[i]);
+  }
+
+  if(resposta)
+  {
+    printf("O valor %d EXISTE no arranjo a partir da posicao %d.\n", valor, posicao);
+  }
+  else
+  {
+    printf("O valor %d NAO existe no arranjo a partir da posicao %d.\n", valor, posicao);
+  }
+}
+int *exercicio09 (chars Filename, int *ref)
+{
+  FILE* arquivo = fopen (Filename, "rt");
+  int *array = NULL;
+  int n = 0;
+  int x = 0;
+  int y = 0;
+
+  if(arquivo == NULL)
+  {
+    G_printERROFILE();
+  }
+  else
+  {
+    fscanf(arquivo, "%d", &n);
+    *ref = n;
+    array = malloc(n * sizeof(int));
+    
+    if(array == NULL)
+    {
+      printf("ERRO ao alocar memoria para arranjo.\n");
+    }
+    else
+    {
+      while (!feof(arquivo) && x < n)
+      {
+        fscanf(arquivo, "%d", &y);
+        array[x] = y;
+        x++;
+      }
+    }
+  }
+  fclose(arquivo);
+  return (array);
+}
+
+int exercicio09_1 (int *array, int n, int posicao, int valor)
+{
+  int resposta = 0;
+  int i = posicao;
+  int count = 0;
+  while(i < n)
+  {
+    if (array[i] == valor)
+    {
+      resposta = i;
+      count++;
+    }
+    i++;
+  }
+  if(count >= 1)
+  {
+    return resposta;
+  }
+  else
+  {
+    return -1;
+  }
+}
+
+void method_09(void) 
+{
+  int *array = NULL;
+  int n = 0;
+  array = exercicio09("DADOS.TXT", &n);
+
+  // G_printarray(n, array);
+  
+  int valor = G_readint("Digite o valor para ser procurado no array: ");
+  int posicao = G_readint("Digite a posicao para comecar a busca: ");
+
+  int resposta = exercicio09_1(array, n, posicao, valor);
+
+  G_printarray(n, array);
+  if(resposta > 0)
+  {
+    printf("\na posicao que o valor foi encontrada e: %d", resposta);
+  }
+  else
+  {
+    printf("\nO valor nao foi encontrado no array.\n");
+  }
+}
+
+int *exercicio10 (chars Filename, int *ref)
+{
+  FILE* arquivo = fopen (Filename, "rt");
+  int *array = NULL;
+  int n = 0;
+  int x = 0;
+  int y = 0;
+
+  if(arquivo == NULL)
+  {
+    G_printERROFILE();
+  }
+  else
+  {
+    fscanf(arquivo, "%d", &n);
+    *ref = n;
+    array = malloc(n * sizeof(int));
+
+    if(array == NULL)
+    {
+      printf("ERRO ao alocar memoria para array.\n");
+    }
+    else
+    {
+      while (!feof(arquivo) && x < n)
+      {
+        fscanf(arquivo, "%d", &y);
+        array[x] = y;
+        x++;
+      }
+    }
+  }
+  fclose(arquivo);
+  return (array);
+}
+
+int exercicio10_1 (int *array, int n, int valor, int posicao)
+{
+  int count = 0;
+  int i = 0;
+  // G_printarray(n, array);
+  for(int i = posicao; i < n; i++)
+  {
+    if(array[i] == valor)
+    {
+      count++;
+    }
+  }
+  return count;
+}
+void method_10(void) 
+{
+  int *array = NULL;
+  int n = 0;
+  int count = 0;
+  array = exercicio10("DADOS.TXT", &n);
+
+  int valor = G_readint("Digite o valor para ser encontrado no array: ");
+  int posicao = G_readint("Digite a posicao para comecar a procura do valor: ");
+
+  count = exercicio10_1(array, n, valor, posicao);
+
+  printf("O valor %d foi encontrado %d vezes no array.\n", valor, count);
+}
+
+int *extra11 (int n, int *quantidade)
+{
+  int x = 0;
+  int y = 0;
+  int count = 0;
+  int *array = NULL;
+
+  for(int i = 1; i <= n; i++)
+  {
+    if(n % i == 0 && i % 2 != 0)
+    {
+      count = count + 1;
+    }
+  }
+  // printf("count = %d\n", count);
+  array = malloc(count * sizeof(int));
+  if(array == NULL)
+  {
+    printf("ERRO ao alocar memoria para arranjo.\n");
+  }
+  else
+  {
+    for(int i = 1; i <= n; i++)
+    {
+      if(n % i == 0 && i % 2 != 0)
+      {
+        x = i;
+        array[y] = x;
+        y++;
+      }
+    }
+  }
+  *quantidade = count;
+
+  return (array);
+}
+void method_11e(void) 
+{
+  int *array = NULL;
+  int y = 0;
+  FILE* arquivo = fopen ("DIVISORES.TXT", "wt");
+  int n = G_readint("Digite o numero: ");
+
+  array = extra11(n, &y);
+  G_printarray(y, array);
+
+  if(arquivo == NULL)
+  {
+    G_printERROFILE();
+  }
+  else
+  {
+    fprintf(arquivo, "%d\n", y);
+    for(int i = 0; i < y; i++)
+    {
+      fprintf(arquivo, "%d\n", array[i]);
+    }
+    fclose(arquivo);
+    printf("\nDados gravados com sucesso em DIVISORES.TXT.\n");
+  }
+}
+
+char *extra12 (chars Filename)
+{
+  // char *string;
+  // int tamanho = 0;
+  // FILE* arquivo = fopen (Filename, "rt");
+  // if(arquivo == NULL)
+  // {
+  //   G_printERROFILE();
+  // }
+  // else
+  // {
+  //   while (!feof(arquivo))
+  //   {
+  //     fscanf(arquivo, "%s", string);
+  //     printf("%s\n", string);
+  //     // if(string[0] != 'c' && string[0] != 'C')
+  //     // {
+  //     //   if(string[tamanho] != 'c' && string[tamanho] != 'C')
+  //     //   {
+  //     //     return string;
+  //     //     printf("%s\n", string);
+  //     //   }
+  //     // }
+  //   }
+  // }
+}
+void method_12e(void) 
+{
+  // // char *string;
+  // // string = extra12("PALAVRAS.TXT");
+  // extra12("PALAVRAS.TXT");
+
+  // // printf("%s\n", string);
+
+}
+
+int main(int argc, char *argv[]) {
+
+  int opcao = 0;
+
+  printf("%s\n", "Estudo dirigido 08");
+  printf("%s\n", "Autor: Gabriel Xavier Borges");
+  printf("\n");
+
+  do {
+
+    printf("\n%s\n", "Opcoes:");
+    printf("\n%s", "0 - Terminar");
+    printf("\n%s", "1 - Method_01");
+    printf("\n");
+    printf("\n%s", "2 - Method_02");
+    printf("\n");
+    printf("\n%s", "3 - Method_03");
+    printf("\n");
+    printf("\n%s", "4 - Method_04");
+    printf("\n");
+    printf("\n%s", "5 - Method_05");
+    printf("\n");
+    printf("\n%s", "6 - Method_06");
+    printf("\n");
+    printf("\n%s", "7 - Method_07");
+    printf("\n");
+    printf("\n%s", "8 - Method_08");
+    printf("\n");
+    printf("\n%s", "9 - Method_09");
+    printf("\n");
+    printf("\n%s", "10 - Method_10");
+    printf("\n");
+    printf("\n%s", "11 - Method_11e");
+    printf("\n");
+    printf("\n%s", "12 - Method_12e");
+    printf("\n");
+
+    printf("\n%s", "Opcao = ");
+    scanf("%d", &opcao);
+    getchar();
+
+    printf("\n%s%d", "Opcao = ", opcao);
+    printf("\n");
+
+    switch (opcao) {
+    case 0:
+      break;
+    case 1:
+      method_01();
+      break;
+    case 2:
+      method_02();
+      break;
+    case 3:
+      method_03();
+      break;
+    case 4:
+      method_04();
+      break;
+    case 5:
+      method_05();
+      break;
+    case 6:
+      method_06();
+      break;
+    case 7:
+      method_07();
+      break;
+    case 8:
+      method_08();
+      break;
+    case 9:
+      method_09();
+      break;
+    case 10:
+      method_10();
+      break;
+    case 11:
+      method_11e();
+      break;
+    case 12:
+      method_12e();
+      break;
+    default:
+      printf("\nERRO: Opcao invalida.\n");
+      break;
+    }
+  } while (opcao != 0);
+
+  printf("\n\nApertar ENTER para terminar.");
+  getchar();
+  return (0);
+}
---
+++ 08/gxb.h
@@ -0,0 +1,418 @@
+#include <math.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+
+typedef char* chars;
+
+/*
+  Funcao para ler um inteiro
+  @param texto a ser imprimido na hora da leitura
+  @return numero tipo inteiro
+*/
+
+int G_readint(char *text) {
+  int x = 0;
+  printf("%s", text);
+  scanf("%d", &x);
+  getchar();
+  return (x);
+}
+
+/*
+  Funcao para ler um double
+  @param texto a ser imprimido na hora da leitura
+  @return numero tipo double
+*/
+
+double G_readdouble(char *text) {
+  double x = 0;
+  printf("%s", text);
+  scanf("%lf", &x);
+  getchar();
+  return (x);
+}
+
+/*
+  Funcao para ler um float
+  @param texto a ser imprimido na hora da leitura
+  @return numero tipo float
+*/
+
+float G_readfloat(char *text) {
+  float x = 0;
+  printf("%s", text);
+  scanf("%f", &x);
+  getchar();
+  return (x);
+}
+
+/*
+  Funcao para ler um caractere
+  @param texto a ser imprimido na hora da leitura
+  @return caractere
+*/
+
+char G_readchar(char *text) {
+  char x = '0';
+  printf("%s", text);
+  scanf("%c", &x);
+  getchar();
+  return (x);
+}
+
+/*
+  Funcao para ler uma string
+  @param texto a ser imprimido na hora da leitura
+  @return string
+*/
+
+char* G_readstring(char *text)
+{
+  char *x = (char*) malloc(100 * sizeof(char));
+  printf("%s", text);
+  scanf("%s", x);
+  getchar();
+  return (x);
+}
+
+
+// char* G_readln(char *text)
+// {
+//   //substituir /n por /0
+// }
+
+/*
+  Funcao para verificar se um numero e par
+  @param numero para ser verificado
+  @return true ou false
+*/ 
+
+bool G_isPar(int x) {
+  if (x % 2 == 0) {
+    return true;
+  }
+  return false;
+}
+
+/*
+  Funcao para verificar se um numero esta em um determinado intervalo fechado
+  @param numero a ser verificado e extremos
+  @return true ou false
+*/ 
+
+bool G_fIntervalo(int x, int a, int b) {
+  if (x >= a && x <= b) {
+    return true;
+  }
+  return false;
+}
+
+/*
+  Funcao para verificar se um numero esta em um determinado intervalo aberto
+  @param numero a ser verificado e extremos
+  @return true ou false
+*/ 
+
+bool G_aIntervalo(int x, int a, int b) {
+  if (x > a && x < b) {
+    return true;
+  }
+  return false;
+}
+
+/*
+  Funcao para verificar se um caracter e uma letra maiuscula
+  @param caracter a ser verificado
+  @return true ou false
+*/
+
+bool G_isMaiuscula (char x)
+{
+  if(x >= 'A' && x <= 'Z')
+  {
+    return true;
+  }
+  return false;
+}
+
+/*
+  Funcao para verificar se um caracter e uma letra minuscula
+  @param caracter a ser verificado
+  @return true ou false
+*/
+
+bool G_isMinuscula (char x)
+{
+  if(x >= 'a' && x <= 'z')
+  {
+    return true;
+  }
+  return false;
+}
+
+/*
+  Funcao para verificar se um caracter e um digito
+  @param caracter a ser verificado
+  @return true ou false
+*/
+
+bool G_isDigit (char x)
+{
+  if(x >= '0' && x <= '9')
+  {
+    return true;
+  }
+  return false;
+}
+
+/*
+  Funcao para verificar se um caracter e um caracter especial
+  @param caracter a ser verificado
+  @return true ou false
+*/
+
+bool G_isSpecial (char x)
+{
+  if(G_isDigit(x) != 1 && G_isMaiuscula(x) != 1 && G_isMinuscula(x) != 1) // ou = 0 
+  {
+    return true;
+  }
+  return false;
+}
+
+/*
+  Funcao para retornar uma potencia de x^y
+  @param base e expoente 
+  @return potencia
+*/
+
+int G_pot (int x, int y)
+{
+  if(y == 0)
+  {
+    return 1;
+  }
+  else
+  {
+    return x * G_pot(x, y - 1);
+  }
+}
+
+/*
+  Funcao para retornar uma potencia double de x^y
+  @param base(double) e expoente 
+  @return potencia
+*/
+
+double G_pot_real (double x, int y)
+{
+  if(y == 0)
+  {
+    return 1;
+  }
+  else
+  {
+    return x * G_pot(x, y - 1);
+  }
+}
+
+/*
+  Funcao para retornar o fatorial de um numero
+  @param numero
+  @return fatorial
+*/
+
+int G_fac (int x)
+{
+  if(x == 0 || x == 1)
+  {
+    return 1;
+  }
+  else
+  {
+    return x * G_fac(x - 1);
+  }
+}
+
+/*
+  Funcao para retornar a sequencia N de fibonacci
+  @param  numero da sequencia
+  @return numero N da sequencia
+*/
+
+int G_fibonacci (int n)
+{
+  int resposta = 0;
+  if(n == 1 || n == 2)
+  {
+    resposta = 1;
+  }
+  else
+  {
+    if(n > 2)
+    {
+      resposta = G_fibonacci(n - 1) + G_fibonacci(n - 2);
+    }
+  }
+  return resposta;
+}
+
+/*
+  Procedimento para mostrar na tela os elementos de um array
+  @param int = tamanho, int array [] = array para ser percorrido e mostrar os elementos
+*/
+
+void G_printarray (int n, int array [])
+{
+  int x = 0;
+  for(int i = 0; i < n; i++)
+  {
+    x = array[i];
+    printf("%d ", x);
+  }
+}
+
+/*
+  Procedimento para ler dados e armazerar em um array de tamanho N
+  @param int n = tamanho, int array [] = lugar a ser armazenado os valores de N
+*/
+
+void G_readarray (int n, int array [])
+{
+  int x = 0;
+  for(int i = 0; i < n; i++)
+  {
+    printf("Elemento %d = ", i);
+    scanf("%d", &x);
+    array[i] = x;
+  }
+}
+
+/*
+  Funcao para somar elementos inteiros dentro de um arranjo
+  @param int n = tamanho, int array [] = elementos do array
+  @return soma dos elementos 
+*/
+
+int G_SumIntArray (int n, int array [])
+{
+  int soma = 0;
+  for(int i = 0; i < n; i++)
+  {
+    soma = soma + array[i];
+  }
+  return soma;
+}
+
+// void G_printdoublearray (int n, double array [])
+// {
+//   for(int i = 0; i < n; i++)
+//   {
+//     printf("%lf ", array[i]);
+//   }
+// }
+
+/*
+  Funcao para duplicar um array
+  @param int n = tamanho, int array [] = elementos do array
+  @return um array duplicado
+*/
+
+int *G_dupArray (int n, int array[])
+{
+  // G_printarray(n, array);
+  int *array1 = NULL;
+  int x = 0;
+  array1 = (int *)malloc(n * sizeof(int));
+
+  for(int i = 0; i < n; i++)
+  {
+    x = array[i];
+    array1[i] = x;
+  }
+
+  return ( array1 );
+}
+
+/*
+  Funcao para comparar dois arrays de tamanhos iguais
+  @param int n = tamanho, int array1/int array2[] = elementos do array
+  @return 1 caso os arrays sejam iguais e -1 para caso nao sejam iguais
+*/
+
+int G_compArray (int n, int array1[], int array2[])
+{
+  int a = 0;
+
+  for(int i = 0; i < n; i++)
+  {
+    if(array1[i] == array2[i])
+    {
+      a++;
+    }
+  }
+  
+  if(a == n)
+  {
+    return 1;
+  }
+  else if(a > n || n > a)
+  {
+    return -1;
+  }
+}
+
+/*
+  Funcao para copiar um array para o outro
+  @param int n = tamanho, int array [] = elementos do array
+  @return um novo array com os elementos dos dois arrays
+*/
+
+int *G_cpyArray(int n, int array[], int array1[])
+{
+  int *newArray = NULL;
+
+  newArray = (int *)malloc((n*2) * sizeof(int));
+
+  for(int i = 0; i < n; i++)
+  {
+    newArray[i] = array[i];
+  } 
+
+  for(int i = n; i < (n*2); i++)
+  {
+    newArray[i] = array1[i - n];
+  }
+
+  return ( newArray );
+}
+
+/*
+  Funcao para verificar o tamanho de um array
+  @param int array []
+  @return tamanho
+*/
+
+/*
+  
+*/
+int G_arrayLen (int *array)
+{
+  // int count = 0;
+  // printf("sizeof = %d\n", sizeof(array));
+  // for(int i  = 1; i < sizeof(array); i++)
+  // {
+  //   count++;
+  //   // printf("%d\n", count);
+  // }
+
+  // return ( count );
+}
+
+void G_printERROFILE ( void )
+{
+  printf("Erro ao abrir o arquivo.\n");
+}

bb567b4e559d910babb60e5af9a42db196a63770
gabsnim <gabrielxb110@gmail.com>
2024-06-10 19:40:04 -0300

    init

---
+++ 15/C++/Erro.hpp
@@ -0,0 +1,10 @@
+#include <string>
+
+class Erro
+{
+private:
+
+public:
+
+};
+
--- 15/C++/string.hpp
+++ 15/C++/string.hpp
@@ -1,8 +1,23 @@
 #include <iostream>
 #include <cstdlib>
+#include "Erro.hpp"
 
-
-class MyString
+class NODE
 {
+    public:
+    int data;
+    class NODE *next;
 
+    NODE (int x)
+    {
+        this->data = x;
+        next = nullptr;
+    }
+
+};
+
+
+class MyString : public Erro
+{
+      
 };
